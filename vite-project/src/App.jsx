7b4459a4f87c49502001ceda6d5c20c3d764621a
abhi835 <abhishek835.hitit2020@gmail.com>
2025-02-07 09:40:20 +0530

    added vite

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Youtube from "./youtube.jsx";
+function App() {
+  
+
+  return (
+    <>
+      <h1>in Vite project</h1>
+      <h1>in Vite project</h1>
+      <Youtube/>
+    </>
+  );
+}
+
+export default App
