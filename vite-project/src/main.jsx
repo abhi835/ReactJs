461b8e1236a7a9aa405ab38f8c779a4e31ba0df5
abhi835 <abhishek835.hitit2020@gmail.com>
2025-02-08 10:41:49 +0530

    add index.html main.jsx

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,42 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+function MyApp(){
+    const username = "abhishek"
+    return (
+        <div>
+            <h1>Custom React App</h1>
+            <h1>Custom React App {username}</h1>
+        </div>
+    )
+}
+
+const AnotherElement = (
+    <a href='http://google.com' target='_blank'>Visit Google</a>
+)
+
+const AreactElement= React.createElement(
+    'a',
+    {href:'http://google.com',target:"_blank"},
+    'Click to visit google'
+)
 
 createRoot(document.getElementById('root')).render(
  
-    <App />
+    <MyApp/>
+    // AreactElement
 
 )
